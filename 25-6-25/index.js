11625ec4e067a27f7a7d57620ef4e183f1783416
Pinki543 <bhargaviallapa@gmail.com>
2025-06-26 04:31:35 +0530

    Add files via upload

---
+++ 25-6-25/index.js
@@ -0,0 +1,32 @@
+var taskToDo = document.getElementById("taskbar")
+document.getElementById("btn").addEventListener("click",addTask)
+function addTask(){
+    var taskName =taskToDo.value
+    if(taskName == ""){
+        alert("Empty Input");
+        return
+    }
+    var listItem= document.createElement("li");
+    var spanTag= document.createElement("span");
+    var delBtn=document.createElement("button");
+    spanTag.textContent=taskName;
+    delBtn.style.backgroundColor= "red";
+    delBtn.style.color="white";
+    delBtn.innerText="Delete";
+    listItem.appendChild(spanTag)
+    listItem.appendChild(delBtn)
+    document.getElementById("tasklist").appendChild(listItem)
+    delBtn.addEventListener('click' ,deleteTask)
+}
+
+function deleteTask(event){
+    console.log(event.target.parentNode.remove())
+}
+document.getElementById("btn").addEventListener("keypress",function(e){
+    if (e.key ==="Enter"){
+        addTask()
+    }
+}
+)
+
+
